8dc1965fee39d2b7945839c29c139770e197f760
Enric Moreu <enric.moreu.filella@gmail.com>
2019-08-28 01:34:06 +0100

    Pose to cubes first appproach

--- src/blenderutils.py
+++ src/blenderutils.py
@@ -1,22 +1,46 @@
 import bpy
+import json
+import os
 
 #Not working
 def reset_blend():
-    bpy.ops.wm.read_factory_settings()
+	bpy.ops.wm.read_factory_settings()
 
-    for scene in bpy.data.scenes:
-        for obj in scene.objects:
-            scene.objects.unlink(obj)
+	for scene in bpy.data.scenes:
+		for obj in scene.objects:
+			scene.objects.unlink(obj)
 
-    for bpy_data_iter in (
-            bpy.data.objects,
-            bpy.data.meshes,
-            bpy.data.lamps,
-            bpy.data.cameras,
-            ):
-        for id_data in bpy_data_iter:
-            bpy_data_iter.remove(id_data)
+	for bpy_data_iter in (
+			bpy.data.objects,
+			bpy.data.meshes,
+			bpy.data.lamps,
+			bpy.data.cameras,
+			):
+		for id_data in bpy_data_iter:
+			bpy_data_iter.remove(id_data)
 
 def remove_startup_cube():
-    objs = bpy.data.objects
-    objs.remove(objs["Cube"], do_unlink=True)
+	objs = bpy.data.objects
+	objs.remove(objs["Cube"], do_unlink=True)
+
+def parse_json_3d(path, bone):
+	with open(path, 'r') as f:
+		distros_dict = json.load(f)
+
+	return [distros_dict['people'][0]['pose_keypoints_2d'][bone],
+	distros_dict['people'][0]['pose_keypoints_2d'][bone+1],
+	distros_dict['people'][0]['pose_keypoints_2d'][bone+2]
+	]
+
+
+def get_bone_at_frame(path, bone, frame):
+	files = []
+	for r, d, f in os.walk(path):
+		for file in f:
+			if '.json' in file:
+				files.append(os.path.join(r, file))
+	return parse_json_3d(os.path.join(r,f[frame]), bone)
+
+def get_total_frames(path):
+	file_count = next(os.walk(path))[2]
+	return len(file_count)
--- src/pose2cubes.py
+++ src/pose2cubes.py
@@ -1,6 +1,13 @@
 import bpy
 from random import randint,random
 import src.blenderutils as blenderutils
+import numpy as np
+
+MAX_BONES=74
+keypoints_path='/pose2char/data/keypoints/andrea'
+total_frames= 500
+# blenderutils.get_total_frames(keypoints_path)
+# import pdb;pdb.set_trace()
 
 blenderutils.remove_startup_cube()
 
@@ -12,29 +19,32 @@ group = bpy.data.groups['CubeXGroup']
 
 def main():
     start_pos=(0,0,0)
-    for i in range(1):
-        add_box(i);
+    for i in range(0,MAX_BONES):
+        add_box(i)
    
 def add_box(i):
     #generate our object
-    bpy.ops.mesh.primitive_cube_add(location = [0,0,0])
+    bpy.ops.mesh.primitive_cube_add(radius=0.2,location = [0,0,0])
     #Select the active object (last added is always the active one
     obj = bpy.context.scene.objects.active
-    object_random_location_animation(obj,-3,3)
+    object_location_animation(obj,i)
     object_random_color(obj,i)
     obj.select = True
     bpy.ops.object.group_link(group="CubeXGroup")
 
 #End AddBox
 
-def object_random_location_animation(obj,min,max):
+def object_location_animation(obj, bone):
     positions =  []
-    for i in range(0,4):
-        positions.append([ randint(min,max) for axis in 'xyz'])
+    for frame in range(0,total_frames):
+            position = blenderutils.get_bone_at_frame(keypoints_path, int(bone/3),frame)
+        #     import pdb;pdb.set_trace()
+            positions.append(np.array(position)/400)
         
     frame_num = 0
     for position in positions:
         bpy.context.scene.frame_set(frame_num)
+        print(frame_num, bone)
         obj.location = position
         obj.keyframe_insert(data_path="location",index = -1)
 
@@ -51,10 +61,10 @@ def  object_random_color(obj,i):
     #You can select colours from a list of predefined colours
 #End  object_random_color
 
-main();
+main()
 
 #save images
-for i in range(0,100):
+for i in range(0,total_frames):
         bpy.context.scene.frame_current = i
         bpy.context.scene.render.image_settings.file_format = 'PNG'
         bpy.context.scene.render.filepath = "/pose2char/output/" + str(i)
